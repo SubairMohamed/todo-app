d0f18cfa4c604e1b139d4910f8c4074154d5b6d8
subair44 <subairmohamed44@gmail.com>
2022-03-12 11:28:52 -0800

    todo-first

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 
 function App() {
   return (
-    <div>
-      <h1>hello world</h1>
+    <div className="flex min-h-screen">
+      {/* left side  */}
+      <aside id="left" className="bg-orange-500 w-20 lg:w-28 flex-none">
+        aside left
+      </aside>
+
+      {/* Main */}
+      <section id="main" className="bg-blue-500 grow">
+        <header>header</header>
+        <main>main</main>
+      </section>
+
+      {/* right side  */}
+      <aside id="right" className="bg-green-500 sm:hidden md:block md:w-80 lg:w-96">
+        aside right
+      </aside>
     </div>
   );
 }
